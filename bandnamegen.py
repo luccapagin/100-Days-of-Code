dcfd3eb98cfa38901b6908d45be760c0259a037f
lucca <luccap2002@gmail.com>
2024-06-14 20:49:44 -0300

    Day 1 - Band Name Generator
    Simple Band Name Generator in Python. In this project, I applied the concepts of Printing, String Manipulation and Variables.

---
+++ bandnamegen.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator!\n")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+band_name = (city + " " + pet)
+print("The name of your band is " + band_name)
+
